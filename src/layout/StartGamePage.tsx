cc9230691f438641d02725bba87217e882546dfd
Nick-M1 <nicholasmalloura@gmail.com>
2023-04-03 11:19:00 +0100

    metadata

--- src/layout/StartGamePage.tsx
+++ src/layout/StartGamePage.tsx
@@ -2,6 +2,7 @@ import HeaderComponent from "./HeaderComponent";
 import UserprofileDropdown from "../components/authentication/UserprofileDropdown";
 import {User} from "firebase/auth";
 import MenuLayout from "./MenuLayout";
+import {Link} from "react-router-dom";
 
 type Props = {
     user: User
@@ -17,8 +18,12 @@ export default function StartGamePage({ user, text, buttonText, buttonHandler }:
                 { text }
             </h1>
 
-            <div className='mx-auto w-fit pb-12'>
-                <button onClick={buttonHandler} className='btn-primary'>
+            <div className='mx-auto w-fit pb-11 grid grid-cols-3 space-x-2'>
+                <Link to='/' className='btn-secondary rounded-md'>
+                    Go Back
+                </Link>
+
+                <button onClick={buttonHandler} className='btn-primary col-span-2'>
                     { buttonText }
                 </button>
             </div>
