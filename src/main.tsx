e65da8f450e339b6af80e1b6cce93088e5ea0a9b
Nick-M1 <nicholasmalloura@gmail.com>
2023-04-03 11:06:39 +0100

    initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,41 +1,29 @@
 import React, {lazy, Suspense} from 'react'
 import ReactDOM from 'react-dom/client'
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './index.css'
 import ErrorPage from "./layout/ErrorPage";
-import LoadingPage from "./layout/LoadingPage";
 import BlankPage from "./layout/BlankPage";
-import FallbackProvider from "./composables/FallbackProvider";
 
 const HomePage = lazy(() => import("./routes/HomePage"))
 const GameroomPage = lazy(() => import("./routes/GameroomPage"))
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: (
-            <FallbackProvider>
-                <HomePage/>
-            </FallbackProvider>
-        ),
-        errorElement: <ErrorPage />,
-    },
-    {
-        path: "/:gameroomId",
-        element: (
-            <FallbackProvider>
-                <GameroomPage/>
-            </FallbackProvider>
-        ),
-        errorElement: <ErrorPage />,
-    },
-]);
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Suspense fallback={<BlankPage/>}>
+                <Routes>
+                    <Route path='/' element={<HomePage/>} errorElement={<ErrorPage/>}/>
+                    <Route path='/:gameroomId' element={<GameroomPage/>} errorElement={<ErrorPage/>}/>
+                </Routes>
+            </Suspense>
+        </BrowserRouter>
+    )
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <App />
     </React.StrictMode>,
 )
