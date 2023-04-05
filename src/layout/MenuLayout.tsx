34d28c26b6f3deb043d00d1924f691d9ae0c0d96
Nick-M1 <nicholasmalloura@gmail.com>
2023-04-05 11:58:03 +0100

    animations

--- src/layout/MenuLayout.tsx
+++ src/layout/MenuLayout.tsx
@@ -8,7 +8,7 @@ export default function MenuLayout({ children }: Props) {
     return (
         <div className='flex flex-col px-2 text-gray-300'>
             <div className='border-b border-neutral-700 w-full py-1'/>
-            <img src='/homepage-icon.svg' alt='home-icon' className='h-[30vh] h-[30dvh] w-fit mx-auto py-3'/>
+            <img src='/homepage-icon.svg' alt='home-icon' className='h-[30vh] w-fit mx-auto py-3'/>
 
             { children }
 
